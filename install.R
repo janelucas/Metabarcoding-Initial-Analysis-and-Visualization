49cb533bdcfcfe785abccdfbc3628dca9d8b08d0
janelucas <50459345+janelucas@users.noreply.github.com>
2019-05-10 11:38:25 -0700

    Create install.R

---
+++ install.R
@@ -0,0 +1,11 @@
+install.packages("ggplot2")
+install.packages("phyloseq")
+install.packages("vegan")
+install.packages("dplyr")
+install.packages("tidyr")
+install.packages("reshape2")
+install.packages("devtools") # if not already installed
+devtools::install_github("biomformat", "joey711")
+install.packages("biomformat")
+install.packages("UpSetR") #plot bargroups/venn dots
+install.packages("ggpubr")
