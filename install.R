b29664fa37f8ffd1369a1b881532466ae33842e7
janelucas <50459345+janelucas@users.noreply.github.com>
2019-05-10 13:40:43 -0700

    Update install.R

--- install.R
+++ install.R
@@ -5,7 +5,6 @@ install.packages("dplyr")
 install.packages("tidyr")
 install.packages("reshape2")
 install.packages("devtools") # if not already installed
-devtools::install_github("biomformat", "joey711")
 install.packages("biomformat")
 install.packages("UpSetR") #plot bargroups/venn dots
 install.packages("ggpubr")
